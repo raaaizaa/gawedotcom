a06056efcd17e5f6ad6696c44484f61b62fbc6aa
raiza r <97652085+raaaizaa@users.noreply.github.com>
2023-01-07 14:15:58 +0700

    Delete freelancer.java

--- freelancer.java
+++
@@ -1,8 +0,0 @@
-package gawedotcom;
-
-public class freelancer extends user{
-    public freelancer(String name, String username, String password){
-        super(name, username, password);
-    }
-    
-}
