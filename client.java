4b2998654004ae758300216b2d44a3fcd0dd080b
raiza r <97652085+raaaizaa@users.noreply.github.com>
2023-01-07 14:15:52 +0700

    Delete client.java

--- client.java
+++
@@ -1,9 +0,0 @@
-package gawedotcom;
-
-public class client extends user {
-    
-    public client(String name, String username, String password){
-        super(name, username, password);
-    }
-    
-}
