8690130230d4cac214008e3671a6693e4b05e6a9
raiza r <97652085+raaaizaa@users.noreply.github.com>
2023-02-03 18:14:18 +0700

    Update userClient.java

--- userClient.java
+++ userClient.java
@@ -5,7 +5,6 @@ package com.mycompany.oop;
  * @author Raiza Rahman
  */
 public class userClient extends user{
-    int freelancerCategory, freelancerNumber, packageNumber;
     
     public userClient(String name, String username, String password){
         super(name, username, password);
